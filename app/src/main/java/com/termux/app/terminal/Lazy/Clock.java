2161644bfe1f5b428f83d5d9dbed0a3424dac758
renzaspiras <aspirasrenz@gmail.com>
2024-04-08 07:42:33 +0800

    Fixed Typo

--- app/src/main/java/com/termux/app/terminal/Lazy/Clock.java
+++ app/src/main/java/com/termux/app/terminal/Lazy/Clock.java
@@ -3,7 +3,7 @@ package com.termux.app.terminal.Lazy;
 import android.os.Handler;
 
 public class Clock {
-    public void CommandDelay(int totalDellayMillis, Runnable action){
-        new Handler().postDelayed(action, (long) totalDellayMillis);
+    public void CommandDelay(int totalDelayMillis, Runnable action){
+        new Handler().postDelayed(action, (long) totalDelayMillis);
     }
 }
